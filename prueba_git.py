b6887b7d3d131f2c73b331e3fbcb28e8a5e3b4cb
Agustin Sigales <AsEa301998@gmail.com>
2022-07-06 01:54:31 -0300

    agregue para probar gi

--- prueba_git.py
+++ prueba_git.py
@@ -2,3 +2,4 @@ print('Hola codigo nuevo')
 print ('agregando otro cambio') #git comit es agregarle una etiqueta diciendo que hicimos
 print ('agregando mas y mas codigo..')
 print (2+2)
+print('nuevo print')
